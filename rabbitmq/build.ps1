3243261feec47670a4673960ecb7c50c117d0a4a
Cort Schaefer <cort@eschaefer.net>
2019-07-14 10:47:36 -0600

    use hyperv isolation to build 1803

--- rabbitmq/build.ps1
+++ rabbitmq/build.ps1
@@ -3,7 +3,7 @@
 Write-Host "Building cortside/${image}:${version}"
 
 $osVersion = "1803"
-docker build --build-arg VERSION=${version} --build-arg OSVERSION=${osVersion} -t cortside/${image}:${version}-${osVersion} -f Dockerfile .
+docker build --isolation hyperv --build-arg VERSION=${version} --build-arg OSVERSION=${osVersion} -t cortside/${image}:${version}-${osVersion} -f Dockerfile .
 
 $osVersion = "1809"
 docker build --build-arg VERSION=${version} --build-arg OSVERSION=${osVersion} -t cortside/${image}:${version}-${osVersion} -f Dockerfile .
