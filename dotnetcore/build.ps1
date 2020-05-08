4e198dd086248335641a8742bb198df99c010d39
Cort Schaefer <cort@eschaefer.net>
2020-05-08 10:30:22 -0600

    remove cleanup stuff that is not needed on the build server

--- dotnetcore/build.ps1
+++ dotnetcore/build.ps1
@@ -2,8 +2,5 @@
 
 Write-Host "Building cortside/${image}:${version}"
 
-docker rm --force dotnetcore
-docker rmi --force cortside/${image}:${version}
-docker image prune --force
 docker build --rm --build-arg VERSION=${version} -t cortside/${image}:${version} -f Dockerfile .
 
