df9090f917fe90105053787a23f318b297636bc7
Cort Schaefer <cort@eschaefer.net>
2023-05-08 13:33:28 -0600

    standardize startup.sh

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 ifconfig
+if [[ -d /settings/$SERVICE_NAME ]]; then
+	cp -R /settings/$SERVICE_NAME/* /app 
+fi
+
+if [[ -f /app/init.sh ]]; then
+	/app/init.sh
+fi
 
 nginx -g "daemon off;"
